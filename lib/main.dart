73586e6795d1757e7fa62585cb57edb4c68d827d
DS Rathore <dsrathorebsw1234@gmail.com>
2021-06-16 16:33:08 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:practice/Pages/Day4.dart';
 import 'package:practice/Pages/Day5.dart';
 import 'package:practice/Pages/Day6.dart';
 import 'package:practice/Pages/Day7.dart';
+import 'package:practice/Pages/Day8.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,10 +24,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         fontFamily: GoogleFonts.robotoSlab().fontFamily,
       ),
-      initialRoute: "/Day7",
+      initialRoute: "/Day8",
       routes: {
         // "/Day9": (day9) => Day9(),
-        // "/Day8": (day8) => Day8(),
+        "/Day8": (day8) => Day8(),
         "/Day7": (day7) => Day7(),
         "/Day6": (day6) => Day6(),
         "/Day5": (day5) => Day5(),
